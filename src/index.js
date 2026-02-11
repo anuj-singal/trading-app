ae3bf60f661f86aafd06aa104c9e5ab864e2881a
Anuj Singal <anujsingal203@gmail.com>
2026-02-11 20:33:20 +0530

    front done

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import HomePage from "./landing_page/home/HomePage";
-// import Signup from "./landing_page/signup/Signup";
-// import AboutPage from "./landing_page/about/AboutPage";
-// import ProductPage from "./landing_page/products/ProductsPage";
-// import PricingPage from "./landing_page/pricing/PricingPage";
-// import SupportPage from "./landing_page/support/SupportPage";
+import Signup from "./landing_page/signup/Signup";
+import AboutPage from "./landing_page/about/AboutPage";
+import ProductPage from "./landing_page/products/ProductsPage";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage";
 
-// import NotFound from "./landing_page/NotFound";
+import NotFound from "./landing_page/NotFound";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
 
@@ -20,12 +20,12 @@ root.render(
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/signup" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer />
   </BrowserRouter>
